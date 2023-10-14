cc54e1e21c3d5f04ccacf609a8731efd8af3baa7
matijamilic18 <matija.redstar@gmail.com>
2023-10-14 12:09:04 +0200

    Drugi korak - commit

--- server/server.go
+++ server/server.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"math/rand"
 	"net"
 	"net/rpc"
+	"sync"
+	"time"
 )
 
 //struct nad kojim cemo pozivati metode
+/*
 type API struct{
 	Values map[string] int;
 }
@@ -32,7 +35,7 @@ func (api *API) Set (request SetRequest, reply *int) error{
 
 
 func (api *API) Get (key string, reply *int) error{
-	 
+
 	val, exists := api.Values[key]
 
 	if !exists{
@@ -41,10 +44,124 @@ func (api *API) Get (key string, reply *int) error{
 	*reply=val
 	return nil
 }
+*/
+
+type Server struct{
+	ID int
+	RPCServer *rpc.Server
+	listener net.Listener
+	peers []*rpc.Client
+	wg *sync.WaitGroup
+}
+
+
+type HeartbeatArgs struct{
+	ID int
+}
+
+
+type HeartbeatReply struct{
+	Reply string
+}
+
+func (s* Server) Heartbeat (args* HeartbeatArgs, reply* HeartbeatReply) error{
+	fmt.Printf("Server %d salje poruku serveru %d\n", args.ID,s.ID)
+	reply.Reply = "Dobijena poruka"
+	return nil
+}
+
+func (s *Server) SendHeartbeats (){
+	for{
+		for targetID:= 0; targetID<5;targetID++{
+			if s.peers[targetID] == nil || targetID==s.ID{
+				continue
+			}
+			arguments := &HeartbeatArgs{s.ID}
+			var reply HeartbeatReply
+
+			err := s.peers[targetID].Call("Server.ReceiveHeartbeat", arguments,&reply)
+			if err!=nil{
+				fmt.Printf("Greska pri slanju")
+			}
+
+		}
+		//povecano vreme timeout-a da bi se lepse videlo u terminalu
+		time.Sleep(time.Duration(1500+rand.Intn(1510)) * time.Millisecond)
+
+	}
+}
+
+func (s *Server) ReceiveHeartbeat(message HeartbeatArgs,  reply* HeartbeatReply) error {
+	
+	reply.Reply = fmt.Sprintf("Server %d je dobio poruku od Servera %d\n", s.ID, message.ID)
+	fmt.Printf(reply.Reply)
+
+
+	return nil
+}
+
+func (s *Server) run (wg *sync.WaitGroup){
+	s.wg=wg
+	wg.Add(1)
+	s.peers = make ([]*rpc.Client, 5)
+	s.RPCServer = rpc.NewServer()
+	s.RPCServer.Register(s)
+	listener , err := net.Listen("tcp" , fmt.Sprintf("localhost:1234%d", s.ID))
+	
+	if err!= nil{
+		fmt.Printf("Greska pri slusanju: %s", err)
+	}
+	
+	s.listener=listener
+	
+
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Greska pri slusanju: %s", err)
+			continue
+		}
+		go s.RPCServer.ServeConn(conn)
+	}
+
+}
 
 
 func main(){
-	service := &API{
+
+	var waitG sync.WaitGroup
+	
+	serverList := make([]*Server, 5)
+
+	//pokretanje servera
+	for i := 0; i < 5; i++ {
+		serverList[i] = &Server{ID: i}
+		go serverList[i].run(&waitG)
+	}
+
+	//povezivanje servera medjusobno
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if i != j {
+				client, err := rpc.Dial("tcp", fmt.Sprintf("localhost:1234%d", j))
+				if err != nil {
+					fmt.Printf("Server %d nije uspeo da se poveze sa serverom %d: %s\n", i, j, err)
+				} else {
+					serverList[i].peers[j] = client
+				}
+			}
+		}
+	}
+
+	//slanje keep-alive poruka
+	for i := 0; i < 5; i++ {
+		go serverList[i].SendHeartbeats()
+	}
+
+	waitG.Wait()
+
+	/*service := &API{
 		Values: make(map[string]int),
 	}
 	
@@ -64,5 +181,5 @@ func main(){
 		}
 		go rpc.ServeConn(conn)
 	}
-	
+	*/
 }
